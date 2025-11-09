d727d3370c9f6929c46e1256f4ab49d67417ab2c
Caleb-Campbell-1 <caleb.campbell@stu.ocsb.ca>
2025-11-09 12:23:45 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,35 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Caleb Campbell
+ * Created on: Nov 2025
+ * This program uses the Bluetooth distance sensor
 */
 
-basic.showString('Hello, World!')
+// setup
+radio.setGroup(0)
+
+
+basic.showIcon(IconNames.Happy)
+// variables
+let distanceToObject: number = 0
+
+
+input.onButtonPressed(Button.A, function () {
+    distanceToObject = 0
+    basic.showIcon(IconNames.Triangle)
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters)
+
+    basic.showNumber(distanceToObject)
+    if ((distanceToObject) <= (20)) {
+        radio.sendString ("Too close!")
+        radio.onReceivedString(function (receivedString) {
+            basic.clearScreen()
+            basic.showString(receivedString)
+            basic.showIcon(IconNames.Happy)})
+    } else {
+    basic.showIcon(IconNames.Heart)
+}
+    }) 
